6d92de3fb290e186507ecb5fbe127d8b1154f11d
arimhan <francoischiltz@hotmail.com>
2016-05-23 16:11:42 +0200

    Added tag conversion
    Former-commit-id: fead2288aed1bc98b8470b35f9614cc9d7bd46d0 [formerly fbb12bc0131884f6e54c511db9d36b9191ea9d91] [formerly 45884752929204a3fba7dfe93310a30542d8fd70] [formerly e4714bbb62b1a62c81b74cc688ee9a4465aee7ba [formerly 67d7f0e5c17fb22663103cfefece683920bd84a3]] [formerly fead2288aed1bc98b8470b35f9614cc9d7bd46d0 [formerly fbb12bc0131884f6e54c511db9d36b9191ea9d91] [formerly 45884752929204a3fba7dfe93310a30542d8fd70] [formerly e4714bbb62b1a62c81b74cc688ee9a4465aee7ba [formerly 67d7f0e5c17fb22663103cfefece683920bd84a3]] [formerly fead2288aed1bc98b8470b35f9614cc9d7bd46d0 [formerly fbb12bc0131884f6e54c511db9d36b9191ea9d91] [formerly 45884752929204a3fba7dfe93310a30542d8fd70] [formerly e4714bbb62b1a62c81b74cc688ee9a4465aee7ba [formerly 67d7f0e5c17fb22663103cfefece683920bd84a3]] [formerly 5b8bc30968fab1726b6588b621853985affcbd37 [formerly a9c6f8daddf3112c86e50b576badee121b2bfb60]]]]
    Former-commit-id: d462d016918428363983d878d53643fb7ae3aeb2
    Former-commit-id: 9729d9edcd3995b7227a24a276a1967e6c63767d
    Former-commit-id: 542e5c9c2fb99b5f9bb627ffc9a821b9e3d2989c
    Former-commit-id: 522d49a6e7912285c1da6f0c45e4821c469d38ea [formerly c3bc1f0c86ebb30eb270dd1d2cdd9d2793320b9c]
    Former-commit-id: fcef969ff897bc1bb7b425f8089b31b913c3aa28
    Former-commit-id: 035d06bd3206ad00a6b78232fd204a914fc53fa5

--- Source/RawParserUWP/RawParserUWP/Model/Parser/NEFParser.cs
+++ Source/RawParserUWP/RawParserUWP/Model/Parser/NEFParser.cs
@@ -19,7 +19,7 @@ namespace RawParser.Model.Parser
         protected byte[] rawData;
         protected byte[] previewData;
 
-        public RawImage parse(Stream file) 
+        public RawImage parse(Stream file)
         {
             //Open a binary stream on the file
             BinaryReader fileStream = new BinaryReader(file);
@@ -28,7 +28,7 @@ namespace RawParser.Model.Parser
             if (fileStream.ReadUInt16() == 0x4D4D)
             {
                 //File is in reverse bit order
-                fileStream = new BinaryReaderBE(file);   
+                fileStream = new BinaryReaderBE(file);
             }
 
             //read the header
@@ -48,31 +48,31 @@ namespace RawParser.Model.Parser
             exif = new IFD(fileStream, (uint)exifoffsetTag.data[0], true); //OK
 
             //optimize (stop ifd from loaoding the makernote
-            
+
             Tag makerNoteOffsetTag;
             exif.tags.TryGetValue(0x927C, out makerNoteOffsetTag);
-                       
+
             makerNote = new NikonMakerNote(fileStream, makerNoteOffsetTag.dataOffset, true);
 
             //Get image data
-            Tag imagepreviewOffsetTags,imagepreviewX,imagepreviewY,imagepreviewSize;
-            makerNote.preview.tags.TryGetValue(0x201,out imagepreviewOffsetTags );
+            Tag imagepreviewOffsetTags, imagepreviewX, imagepreviewY, imagepreviewSize;
+            makerNote.preview.tags.TryGetValue(0x201, out imagepreviewOffsetTags);
             makerNote.preview.tags.TryGetValue(0x11A, out imagepreviewX);
             makerNote.preview.tags.TryGetValue(0x11B, out imagepreviewY);
             makerNote.preview.tags.TryGetValue(0x202, out imagepreviewSize);
 
             //get Preview Data
-           
+
             //get Raw Data
             rawData = new byte[0];
 
             //get the preview data ( faster than rezising )
             fileStream.BaseStream.Position = (uint)imagepreviewOffsetTags.data[0] + makerNoteOffsetTag.dataOffset + 10;
             previewData = fileStream.ReadBytes(Convert.ToInt32(imagepreviewSize.data[0]));
-            
+
 
             //parse to RawImage
-            Dictionary<ushort, Tag>exifTag = parseToStandardExifTag();
+            Dictionary<ushort, Tag> exifTag = parseToStandardExifTag();
             RawImage rawImage = new RawImage(exifTag, rawData, previewData);
             //get the imagedata
 
@@ -82,31 +82,31 @@ namespace RawParser.Model.Parser
         public Dictionary<ushort, Tag> parseToStandardExifTag()
         {
             Dictionary<ushort, Tag> temp = new Dictionary<ushort, Tag>();
-            Dictionary<ushort, ushort> nikonToStandard = new NikonTagDictionnary();
-            Dictionary<ushort, string> standardExifName = new StandardExifNameDictionnary();
-            foreach(ushort nikonTagId in nikonToStandard.Values)
+            Dictionary<ushort, ushort> nikonToStandard = new DictionnaryFromFileUShort(@"Assets\Dic\NikonToStandard.dic");
+            Dictionary<ushort, string> standardExifName = new DictionnaryFromFileString(@"Assets\\Dic\standartExif.dic");
+            foreach (ushort exifTag in standardExifName.Keys)
             {
                 Tag tempTag = null;
-                if(ifd.tags.TryGetValue(nikonTagId, out tempTag))
+                ushort nikonTagId;
+                if (nikonToStandard.TryGetValue(exifTag, out nikonTagId))
                 {
-
+                    if (!ifd.tags.TryGetValue(nikonTagId, out tempTag)
+                        || !makerNote.ifd.tags.TryGetValue(nikonTagId, out tempTag)
+                        || !subifd0.tags.TryGetValue(nikonTagId, out tempTag)
+                        || !subifd1.tags.TryGetValue(nikonTagId, out tempTag))
+                    {
+                        tempTag = new Tag();
+                        tempTag.dataType = 2;
+                        tempTag.data[0] = "";
+                    }
+                    else
+                    {
+                        string t = "";
+                        standardExifName.TryGetValue(exifTag, out t );
+                        tempTag.displayName = t;
+                    }
+                    temp.Add(nikonTagId, tempTag);
                 }
-                else if (makerNote.ifd.tags.TryGetValue(nikonTagId, out tempTag)) //search in MakerNote
-                {
-
-                }
-                else if (subifd0.tags.TryGetValue(nikonTagId, out tempTag))
-                {
-
-                }
-                else if (subifd1.tags.TryGetValue(nikonTagId, out tempTag))
-                {
-
-                }else
-                {
-                    continue;
-                }
-                //tag is found
             }
             return temp;
         }
