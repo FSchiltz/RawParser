6ec5833819a7a5f0b2145e1fa34cb2c529c35c28
arimhan <francoischiltz@hotmail.com>
2016-05-23 21:52:04 +0200

    preparation for Raw decoding
    Former-commit-id: 63a7c91b286a9fffb2ed8de0667b8dd57c465142 [formerly 0e2e000b7ff9a9966757da0e83c84924c50eeb21] [formerly 4d6ab7bff2eaf86d5882be1435df82d6102bb672] [formerly ca814f1309c06cec4f7038e7192141132f290988 [formerly a693ee8337a66a006d660d6fb577d580f60c703b]] [formerly 63a7c91b286a9fffb2ed8de0667b8dd57c465142 [formerly 0e2e000b7ff9a9966757da0e83c84924c50eeb21] [formerly 4d6ab7bff2eaf86d5882be1435df82d6102bb672] [formerly ca814f1309c06cec4f7038e7192141132f290988 [formerly a693ee8337a66a006d660d6fb577d580f60c703b]] [formerly 63a7c91b286a9fffb2ed8de0667b8dd57c465142 [formerly 0e2e000b7ff9a9966757da0e83c84924c50eeb21] [formerly 4d6ab7bff2eaf86d5882be1435df82d6102bb672] [formerly ca814f1309c06cec4f7038e7192141132f290988 [formerly a693ee8337a66a006d660d6fb577d580f60c703b]] [formerly 7b3c68f6b5dfcc6179ba987738925cfb47a4d1f6 [formerly 43b80234912bebba777c8f015ecb50f89e8169af]]]]
    Former-commit-id: ba155a65429e484963cef2f103feea88cfdc2c14
    Former-commit-id: 1dc1acd41e4a30b6ac15bd39b380c8b5a3b8b225
    Former-commit-id: 922eac4b1a887f4b3619428c23fddfe4b4318de0
    Former-commit-id: ab379b43d52b5daff9edc4622fdaa301bc71c314 [formerly 681796ae3d69640997dbe8688ccd6ca48e15b6e8]
    Former-commit-id: ab5784b32576015c6a471a28b5b08a00bd6a7dc1
    Former-commit-id: 92db9a0caa8330397aa4bea126b58a2a496f0990

--- Source/RawParserUWP/RawParserUWP/Model/Format/ImageDisplay/RawImage.cs
+++ Source/RawParserUWP/RawParserUWP/Model/Format/ImageDisplay/RawImage.cs
@@ -10,25 +10,36 @@ namespace RawParser.Model.ImageDisplay
 
     class RawImage
     {
-        private string fileName {get;set;}
+        private string fileName { get; set; }
         public Dictionary<ushort, Tag> exif { get; set; }
         public byte[] imageData { set; get; }
         public byte[] imagePreviewData { get; set; }
+        public uint height;
+        public uint width;
 
-        public RawImage(Dictionary<ushort, Tag> e ,byte[] d, byte[] p)
+        public RawImage(Dictionary<ushort, Tag> e, byte[] d, byte[] p, uint h, uint w)
         {
             exif = e;
             imageData = d;
-            imagePreviewData = p;           
+            imagePreviewData = p;
+            height = h;
+            width = w;
         }
 
-        public SoftwareBitmap getImageAsBitmap()
+        public SoftwareBitmap getImageAsBitmap(bool raw)
         {
             MemoryStream ms = new MemoryStream();
-            ms.Write(imagePreviewData, 0, imagePreviewData.Length);
+            if (raw)
+            {
+                ms.Write(imageData, 0, imageData.Length);
+            }
+            else
+            {
+                ms.Write(imagePreviewData, 0, imagePreviewData.Length);
+            }
             ms.Position = 0; //reset the stream after populate
             var decoder = BitmapDecoder.CreateAsync(ms.AsRandomAccessStream());
-            
+
             Task t = Task.Run(() =>
             {
                 while (decoder.Status == AsyncStatus.Started) { }
@@ -38,19 +49,19 @@ namespace RawParser.Model.ImageDisplay
             {
                 throw decoder.ErrorCode;
             }
-            
+
             var bitmapasync = decoder.GetResults().GetSoftwareBitmapAsync();
             t = Task.Run(() =>
-            {
-                while (bitmapasync.Status == AsyncStatus.Started) { }
-            });
+                    {
+                        while (bitmapasync.Status == AsyncStatus.Started) { }
+                    });
             t.Wait();
             if (bitmapasync.Status == AsyncStatus.Error)
             {
                 throw bitmapasync.ErrorCode;
             }
-                                     
-            return bitmapasync.GetResults(); ;            
+
+            return bitmapasync.GetResults(); ;
         }
     }
 }
