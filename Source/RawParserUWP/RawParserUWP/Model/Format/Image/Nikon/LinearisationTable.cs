4b1b02f4ab4ae32ffb72eb9a99cddf0193d70732
arimhan <francoischiltz@hotmail.com>
2016-05-25 19:54:21 +0200

    Added linear table
    Former-commit-id: 9060cb1d223c02f4d9f209b56d45618a7f44ac7c [formerly 66cc9588f3acbdaaf21dd3376395bee7f621d8b5]
    Former-commit-id: 208e7335e9934341605de0f709e353365a70705d [formerly f54b26da608e1ddae59984f2ff424f0a8c6d4bec]
    Former-commit-id: c45baff721daa6c48aef66f07053e346b5992eb8
    Former-commit-id: d460318c547cbd9e28796d5fea2a73851af0bf72

---
+++ Source/RawParserUWP/RawParserUWP/Model/Format/Image/Nikon/LinearisationTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace RawParser.Model.Parser
+{
+    internal class LinearisationTable
+    {
+        public byte version0;
+        public byte version1;
+        public short[][] vpreds;
+        public short curveSize;
+        public short[] curve;
+        public short splitValue;
+
+        /*
+         * Source from DCRaw
+         * 
+         */
+        public LinearisationTable(object[] table, ushort compressionType)
+        {
+            //get the version
+            version0 = (byte)table[0];
+            version1 = (byte)table[1];
+
+            //get the 4 vpreds
+
+            vpreds = new short[2][];
+            vpreds[0] = new short[2];
+            vpreds[1] = new short[2];
+
+            //(when ver0 == 0x49 || ver1 == 0x58, fseek (ifp, 2110, SEEK_CUR) before)
+            if (version0 == 0x49 || version1 == 0x58)
+            {
+                //fseek(ifp, 2110, SEEK_CUR) before));
+
+            }
+            vpreds[0][0] = BitConverter.ToInt16(new byte[2] { (byte)table[2], (byte)table[3] }, 0);
+            vpreds[0][1] = BitConverter.ToInt16(new byte[2] { (byte)table[4], (byte)table[5] }, 0);
+            vpreds[1][0] = BitConverter.ToInt16(new byte[2] { (byte)table[6], (byte)table[7] }, 0);
+            vpreds[1][1] = BitConverter.ToInt16(new byte[2] { (byte)table[8], (byte)table[9] }, 0);
+
+            //get the curvesize
+            curveSize = Convert.ToInt16(table[10]);
+
+            if (curveSize == 257 && compressionType == 4)
+            {
+                curveSize = (short)(1 + curveSize * 2);
+            }
+            curve = new short[curveSize];
+            for(int i = 0; i < curveSize; i++)
+            {
+                curve[i] = (byte)table[i + 12];
+            }
+
+            if(compressionType == 4)
+            {
+                splitValue = BitConverter.ToInt16(new byte[2] { (byte)table[562], (byte)table[563] }, 0); 
+            }
+            
+        }
+    }
+}
--- Source/RawParserUWP/RawParserUWP/Model/Parser/NEFParser.cs
+++ Source/RawParserUWP/RawParserUWP/Model/Parser/NEFParser.cs
@@ -2,6 +2,7 @@
 using RawParser.Model.Format.Nikon;
 using RawParser.Model.ImageDisplay;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
@@ -16,7 +17,7 @@ namespace RawParser.Model.Parser
         protected IFD exif;
         protected NikonMakerNote makerNote;
 
-        protected byte[] rawData;
+        protected BitArray rawData;
         protected byte[] previewData;
 
         public RawImage parse(Stream file)
@@ -61,10 +62,11 @@ namespace RawParser.Model.Parser
             if (!subifd0.tags.TryGetValue(0x11B, out imagepreviewY)) throw new FormatException("File not correct");
             if (!subifd0.tags.TryGetValue(0x202, out imagepreviewSize)) throw new FormatException("File not correct");
 
-            Tag imageRAWOffsetTags, imageRAWWidth, imageRAWHeight, imageRAWSize, imageRAWCompressed;
+            Tag imageRAWOffsetTags, imageRAWWidth, imageRAWHeight, imageRAWSize, imageRAWCompressed, imageRAWDepth;
             if (!subifd1.tags.TryGetValue(0x0111, out imageRAWOffsetTags)) throw new FormatException("File not correct");
             if (!subifd1.tags.TryGetValue(0x0100, out imageRAWWidth)) throw new FormatException("File not correct");
             if (!subifd1.tags.TryGetValue(0x0101, out imageRAWHeight)) throw new FormatException("File not correct");
+            if (!subifd1.tags.TryGetValue(0x0102, out imageRAWDepth)) throw new FormatException("File not correct");
             if (!subifd1.tags.TryGetValue(0x0117, out imageRAWSize)) throw new FormatException("File not correct");
             if (!subifd1.tags.TryGetValue(0x0117, out imageRAWCompressed)) throw new FormatException("File not correct");
 
@@ -74,7 +76,7 @@ namespace RawParser.Model.Parser
 
             //get Raw Data            
             fileStream.BaseStream.Position = (uint)imageRAWOffsetTags.data[0];
-            rawData = fileStream.ReadBytes(Convert.ToInt32(imageRAWSize.data[0]));
+            rawData = new BitArray(fileStream.ReadBytes(Convert.ToInt32(imageRAWSize.data[0])));
 
             //Check if uncompressed
             if ((uint)imageRAWCompressed.data[0] == 34713)
@@ -82,32 +84,62 @@ namespace RawParser.Model.Parser
                 Tag compressionType;
                 if (!makerNote.ifd.tags.TryGetValue(0x0093, out compressionType)) throw new FormatException("File not correct");
                 //uncompress the image
-                rawData = uncompressed(rawData, (int)imageRAWHeight.data[0], (int)imageRAWWidth.data[0], (ushort)compressionType.data[0]);
+                rawData = uncompressed(new BitArray(rawData), (int)imageRAWHeight.data[0], (int)imageRAWWidth.data[0], (ushort)compressionType.data[0], (ushort)imageRAWDepth.data[0]);
             }
             //parse to RawImage
             Dictionary<ushort, Tag> exifTag = parseToStandardExifTag();
-            RawImage rawImage = new RawImage(exifTag, rawData, previewData, (uint)imageRAWHeight.data[0], (uint)imageRAWWidth.data[0]);
+            RawImage rawImage = new RawImage(exifTag, rawData, previewData, (uint)imageRAWHeight.data[0], (uint)imageRAWWidth.data[0], (ushort)imageRAWDepth.data[0]);
 
             return rawImage;
         }
 
-        private byte[] uncompressed(byte[] rawData, int height, int width, ushort compressionType)
+        /*
+         * Only lossless for the moment
+         * 
+         */
+        private BitArray uncompressed(BitArray rawData, int height, int width, ushort compressionType, ushort colordepth)
         {
-            byte[] uncompressedData = new byte[height * width];
+            byte[] uncompressedData = new byte[height * width]; //add pixel*
+            //decompress the linearisationtable
+            Tag lineTag = new Tag();
+            makerNote.ifd.tags.TryGetValue(0x0096, out lineTag);
+            LinearisationTable line = new LinearisationTable(lineTag.data, compressionType);
+
+            //huffman tree for the different copression type
+            byte[][] nikon_tree =
+                {
+                    new byte[]{ 0,1,5,1,1,1,1,1,1,2,0,0,0,0,0,0,	/* 12-bit lossy */
+                      5,4,3,6,2,7,1,0,8,9,11,10,12 },
+                    new byte[]{ 0,1,5,1,1,1,1,1,1,2,0,0,0,0,0,0,	/* 12-bit lossy after split */
+                      0x39,0x5a,0x38,0x27,0x16,5,4,3,2,1,0,11,12,12 },
+                    new byte[] { 0,1,4,2,3,1,2,0,0,0,0,0,0,0,0,0,  /* 12-bit lossless */
+                      5,4,6,3,7,2,8,1,9,0,10,11,12 },
+                    new byte[]{ 0,1,4,3,1,1,1,1,1,2,0,0,0,0,0,0,	/* 14-bit lossy */
+                      5,6,4,7,8,3,9,2,1,0,10,11,12,13,14 },
+                    new byte[]{ 0,1,5,1,1,1,1,1,1,1,2,0,0,0,0,0,	/* 14-bit lossy after split */
+                      8,0x5c,0x4b,0x3a,0x29,7,6,5,4,3,2,1,0,13,14 },
+                    new byte [] { 0,1,4,2,2,3,1,2,0,0,0,0,0,0,0,0,	/* 14-bit lossless */
+                      7,6,8,5,9,4,10,3,11,12,2,0,1,13,14 }
+            };
+
             switch (compressionType)
             {
                 case 1:
                     //lossy
-                    { }
+                    {
+                    }
                     break;
                 case 3:
                     //Lossless
-                    { }
+                    {
+
+                    }
                     break;
                 case 4:
                     //Compression(Compression = 34713) is a Huffman tree and a quantization table. The quantization tables are at 0x8c and 0x96 tag from the MakerNote.
                     //lossy type2
-                    { }
+                    {
+                    }
                     break;
                 default: throw new FormatException("Comrpession Type not correct");
             }
