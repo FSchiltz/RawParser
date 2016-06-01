38eb71e7f4505257a1bb70b8fdeb19e12abd7767
arimhan <francoischiltz@hotmail.com>
2016-06-01 12:16:47 +0200

    Fix emory usage + incorrect param
    Former-commit-id: d31ea30f7a38916d2c38c45fa8a07d3b5816be59 [formerly 09f805f3ab0355070174aabd3058e480f8a9d3c0] [formerly 6902d1943562a92aab524c156a9ca7feecc55e97 [formerly e99053245ab4d32fa80eaec6d184ca99ebfecbf6] [formerly 764651332324f734b81ee3eeee0317fa2e22b662] [formerly e96bc11f51515c1963411e3159c04a6282d9752f [formerly 81f65b938fb9251a5c30f7cf9ac935a0a2c96871]] [formerly 6902d1943562a92aab524c156a9ca7feecc55e97 [formerly e99053245ab4d32fa80eaec6d184ca99ebfecbf6] [formerly 764651332324f734b81ee3eeee0317fa2e22b662] [formerly e96bc11f51515c1963411e3159c04a6282d9752f [formerly 81f65b938fb9251a5c30f7cf9ac935a0a2c96871]] [formerly 6902d1943562a92aab524c156a9ca7feecc55e97 [formerly e99053245ab4d32fa80eaec6d184ca99ebfecbf6] [formerly 764651332324f734b81ee3eeee0317fa2e22b662] [formerly e96bc11f51515c1963411e3159c04a6282d9752f [formerly 81f65b938fb9251a5c30f7cf9ac935a0a2c96871]] [formerly 8f83743e25c950bd7393e170e8cf57544e078c2c [formerly 5fe97c4a8703d40e18c3e4412bce570b7db17af2]]]]]
    Former-commit-id: 07bb9bfe330936725dc9086f4d2ae9ef24fac047
    Former-commit-id: e112586bd0c29f37b31d4b348e52ce1c3382ca46
    Former-commit-id: d7ae0435e325c542f45a84bde579ddcb81d7fd5c
    Former-commit-id: 32a46a954c3b802d656b621270451feced1e5972
    Former-commit-id: c114230a23360c1f3918f8b4b33cc3f187cc3898 [formerly 97346fac543e2cfbb1cf64863b9119bcd4b33009]
    Former-commit-id: e1cd68860cbf7ba5c519dc315b29d36767059118
    Former-commit-id: ab3e3a1a60c257e4290dc4f3958bf8a0c73ebc99

--- Source/RawParserUWP/RawParserUWP/Model/Format/Image/IFD.cs
+++ Source/RawParserUWP/RawParserUWP/Model/Format/Image/IFD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -34,81 +35,99 @@ namespace RawParserUWP.Model.Format.Image
                     temp.dataOffset = fileStream.ReadUInt32();
                 }
 
-                temp.data = new Object[temp.dataCount];
-                long firstPosition = fileStream.BaseStream.Position;
-                if (temp.dataOffset > 1)
+                
+                if (temp.tagId != 0x0096 && temp.tagId != 0x927C)//if makernote or linearisation table, do not get the data ( optimisation)
                 {
-                    var realoffset = temp.dataOffset;
-                    if (relativeOffset)
+                    temp.data = new Object[temp.dataCount];
+                    long firstPosition = fileStream.BaseStream.Position;
+                    if (temp.dataOffset > 1)
                     {
-                        realoffset += offset - 8;
-                        //TODO removemagicvalue and fix
-                    }
-                    
-                    fileStream.BaseStream.Seek(realoffset, SeekOrigin.Begin);
-                    //todocheck if correct
-                }
+                        var realoffset = temp.dataOffset;
+                        if (relativeOffset)
+                        {
+                            realoffset += offset - 8;
+                            //TODO removemagicvalue and fix
+                        }
 
-                for (int j = 0; j < temp.dataCount; j++)
-                {
-                    switch (temp.dataType)
+                        fileStream.BaseStream.Seek(realoffset, SeekOrigin.Begin);
+                        //todocheck if correct
+                    }
+
+                    for (int j = 0; j < temp.dataCount; j++)
                     {
-                        case 1:
-                        case 2:
-                        case 7:
-                            temp.data[j] = fileStream.ReadByte();
-                            break;
-                        case 3:
-                            temp.data[j] = fileStream.ReadUInt16();
-                            break;
-                        case 4:
-                            temp.data[j] = fileStream.ReadUInt32();
-                            break;
-                        case 5:
-                            temp.data[j] = fileStream.ReadDouble();
-                            break;
-                        case 6:
-                            temp.data[j] = fileStream.ReadSByte();
-                            break;
-                        case 8:
-                            temp.data[j] = fileStream.ReadInt16();
-                            if (temp.dataOffset == 0 && temp.dataCount == 1) fileStream.ReadInt16();
-                            break;
-                        case 9:
-                            temp.data[j] = fileStream.ReadInt32();
-                            break;
-                        case 10:
-                            temp.data[j] = fileStream.ReadDouble();
-                            break;
-                        case 11:
-                            temp.data[j] = fileStream.ReadBytes(4);
-                            break;
-                        case 12:
-                            temp.data[j] = fileStream.ReadDouble();
-                            break;
+                        switch (temp.dataType)
+                        {
+                            case 1:
+                            case 2:
+                            case 7:
+                                temp.data[j] = fileStream.ReadByte();
+                                break;
+                            case 3:
+                                temp.data[j] = fileStream.ReadUInt16();
+                                break;
+                            case 4:
+                                temp.data[j] = fileStream.ReadUInt32();
+                                break;
+                            case 5:
+                                temp.data[j] = fileStream.ReadDouble();
+                                break;
+                            case 6:
+                                temp.data[j] = fileStream.ReadSByte();
+                                break;
+                            case 8:
+                                temp.data[j] = fileStream.ReadInt16();
+                                if (temp.dataOffset == 0 && temp.dataCount == 1) fileStream.ReadInt16();
+                                break;
+                            case 9:
+                                temp.data[j] = fileStream.ReadInt32();
+                                break;
+                            case 10:
+                                //Because the nikonmakernote is broken with the tag 0x19 wich is double but offset of zero.
+                                if (temp.dataOffset == 0)
+                                {
+                                    temp.data[j] = .0;
+                                }
+                                else
+                                {
+                                    temp.data[j] = fileStream.ReadDouble();
+                                }
+                                break;
+                            case 11:
+                                temp.data[j] = fileStream.ReadBytes(4);
+                                break;
+                            case 12:
+                                temp.data[j] = fileStream.ReadDouble();
+                                break;
+                        }
+                    }
+
+                    //transform data ToString
+                    if (temp.dataType == 2)
+                    {
+                        string t = Encoding.ASCII.GetString(temp.data.Cast<byte>().ToArray());
+                        temp.data = new Object[1];
+                        temp.data[0] = t;
+                    }
+
+                    if (temp.dataOffset > 1)
+                    {
+                        fileStream.BaseStream.Seek(firstPosition, SeekOrigin.Begin);
+                    }
+                    else if (temp.dataOffset == 0)
+                    {
+                        int k = (int)temp.dataCount * temp.getTypeSize(temp.dataType);
+                        if (k < 4)
+                            fileStream.ReadBytes(4 - k);
                     }
                 }
-
-                //transform data ToString
-                if (temp.dataType == 2)
+                if (!tags.ContainsKey(temp.tagId))
                 {
-                    string t = Encoding.ASCII.GetString(temp.data.Cast<byte>().ToArray());
-                    temp.data = new Object[1];
-                    temp.data[0] = t;
+                    tags.Add(temp.tagId, temp);
                 }
-
-                if (temp.dataOffset > 1)
+                else
                 {
-                    fileStream.BaseStream.Seek(firstPosition, SeekOrigin.Begin);
+                    Debug.WriteLine("tags already exist");
                 }
-                else if (temp.dataOffset == 0)
-                {
-                    int k = (int)temp.dataCount * temp.getTypeSize(temp.dataType);
-                    if (k < 4)
-                        fileStream.ReadBytes(4 - k);
-                }
-
-                tags.Add(temp.tagId, temp);
             }
             nextOffset = fileStream.ReadUInt16();
         }
