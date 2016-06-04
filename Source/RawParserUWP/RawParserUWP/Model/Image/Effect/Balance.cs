4668a33790451d7e096085c151987b3bafa9cde3
arimhan <francoischiltz@hotmail.com>
2016-06-04 19:38:48 +0200

    fix
    Former-commit-id: 17b6f13df7886457c550cea3cdd690248d8d990f [formerly dcf5abc3ba6bd90c742201a226b0b30daacc8f19] [formerly bb943e5c6547f191af58fc193d612ca3893b57ef [formerly 1103c9b8c6fee037c9facaf02d298f4b596a9beb] [formerly bb9085dc9aa55767623be58277675ebec61a76e4] [formerly 489d67d2b04b88682e2f9bc66af3dbfe59e26bb0 [formerly 22cc738fba37d6ac2227dbc5cf651fff90c4c0f9]] [formerly bb943e5c6547f191af58fc193d612ca3893b57ef [formerly 1103c9b8c6fee037c9facaf02d298f4b596a9beb] [formerly bb9085dc9aa55767623be58277675ebec61a76e4] [formerly 489d67d2b04b88682e2f9bc66af3dbfe59e26bb0 [formerly 22cc738fba37d6ac2227dbc5cf651fff90c4c0f9]] [formerly bb943e5c6547f191af58fc193d612ca3893b57ef [formerly 1103c9b8c6fee037c9facaf02d298f4b596a9beb] [formerly bb9085dc9aa55767623be58277675ebec61a76e4] [formerly 489d67d2b04b88682e2f9bc66af3dbfe59e26bb0 [formerly 22cc738fba37d6ac2227dbc5cf651fff90c4c0f9]] [formerly e18c5aa085a5d954f6cf4d5f7c159e5920911cd3 [formerly 6f682f2a028ccc8026f9a04304fa50c20d829ab9]]]]]
    Former-commit-id: 46c27ab673e45c622e53e4d9315fdcfcb7c8a12a
    Former-commit-id: b30ace2826afef8920f35b5a166b9b2bcfa6e919
    Former-commit-id: b9c9cb542c808cc1bc0d4395689507cd150999fe
    Former-commit-id: e70caa9bce284875d5edff4625b462d4d5516dd8
    Former-commit-id: b5b1dfc177e58e72b02e41a29a06471dce621c96 [formerly db3f8bd0e0aefceb6be12978e98f02961bb8dbef]
    Former-commit-id: 23483b3001c7f94462b0fbb7e3976decef601788
    Former-commit-id: 203ab83f3ad7cf3d4c93fddc8987d39744f57ea7

--- Source/RawParserUWP/RawParserUWP/Model/Image/Effect/Balance.cs
+++ Source/RawParserUWP/RawParserUWP/Model/Image/Effect/Balance.cs
@@ -8,68 +8,103 @@ namespace RawParserUWP
         /*
          * Start with a temperature, in Kelvin, somewhere between 1000 and 40000.  (Other values may work,
                  but I can't make any promises about the quality of the algorithm's estimates above 40000 K.)
-    Note also that the temperature and color variables need to be declared as floating - point.
-    */
+            Note also that the temperature and color variables need to be declared as floating - point.
+        */
         public static void whiteBalance(ref RawImage image, int temp, int tint)
         {
-            //TODO caclute the real value andremove transofrmingto8 bit
+            //TODO caclute the real value and remove transforming to 8 bit
             ushort maxValue = 255;
             temp /= 100;
             ushort factor = (ushort)(image.colorDepth - 8);
-            ushort refer;
-            float tempStrength = 0.5f;
+            ushort rRefer, gRefer, bRefer;
+
             if (temp >= 66)
             {
-                for (int i = 0; i < image.width * image.height; i++)
-                {
-                    //red
-                    refer = (ushort)(351.97690566805693
-                        + (0.114206453784165 * (temp - 55))
-                        + ((-40.25366309332127) * Math.Log(temp - 55)));
-                    if (refer < 0) refer = 0;
-                    else if (refer > maxValue) refer = maxValue;
-                    //apply
-                    image.imageData[i * 3] = (ushort)(image.imageData[i * 3] * (1 - tempStrength) + (refer << factor) * tempStrength);
+                //red
+                rRefer = (ushort)(351.97690566805693
+                    + (0.114206453784165 * (temp - 55))
+                    + ((-40.25366309332127) * Math.Log(temp - 55)));
+                if (rRefer < 0) rRefer = 0;
+                else if (rRefer > maxValue) rRefer = maxValue;
 
-                    //green
-                    refer = (ushort)((325.4494125711974)
-                        + (0.07943456536662342 * (temp - 50))
-                        + ((-28.0852963507957) * Math.Log(temp - 50)));
-                    if (refer < 0) refer = 0;
-                    else if (refer > maxValue) refer = maxValue;
-                    image.imageData[(i * 3) + 1] = (ushort)(image.imageData[(i * 3) + 1] * (1 - tempStrength) + (refer << factor) * tempStrength);
+                //green
+                gRefer = (ushort)((325.4494125711974)
+                    + (0.07943456536662342 * (temp - 50))
+                    + ((-28.0852963507957) * Math.Log(temp - 50)));
+                if (gRefer < 0) gRefer = 0;
+                else if (gRefer > maxValue) gRefer = maxValue;
 
-                    //blue
-                    image.imageData[(i * 3) + 2] = (ushort)(image.imageData[(i * 3) + 2] * (1 - tempStrength) + (maxValue << factor) * tempStrength);
-                }
+                //blue
+                bRefer = maxValue;
             }
             else
             {
-                for (int i = 0; i < image.width * image.height; i++)
+                //red                    
+                rRefer = maxValue;
+
+                //green
+                gRefer = (ushort)(((-155.25485562709179))
+                    + ((-0.44596950469579133) * (temp - 2))
+                    + (104.49216199393888 * Math.Log(temp - 2)));
+                if (gRefer < 0) gRefer = 0;
+                else if (gRefer > maxValue) gRefer = maxValue;
+
+                //blue
+                if (temp <= 19) bRefer = 0;
+                else
                 {
-                    //red                    
-                    image.imageData[i * 3] = (ushort)(image.imageData[i * 3] * (1 - tempStrength) + (maxValue << factor) * tempStrength);
-
-                    //green
-                    refer = (ushort)(((-155.25485562709179))
-                        + ((-0.44596950469579133) * (temp - 2))
-                        + (104.49216199393888 * Math.Log(temp - 2)));
-                    if (refer < 0) refer = 0;
-                    else if (refer > maxValue) refer = maxValue;
-                    image.imageData[(i * 3) + 1] = (ushort)(image.imageData[(i * 3) + 1] * (1 - tempStrength) + (refer << factor) * tempStrength);
-
-                    //blue
-                    if (temp <= 19) refer = 0;
-                    else
-                    {
-                        refer = (ushort)(((-254.76935184120902))
-                            + (0.8274096064007395 * (temp - 10))
-                            + (115.67994401066147 * Math.Log(temp - 10)));
-                        if (refer < 0) refer = 0;
-                        else if (refer > maxValue) refer = maxValue;
-                    }
-                    image.imageData[(i * 3) + 2] = (ushort)(image.imageData[(i * 3) + 2] * (1 - tempStrength) + (refer << factor) * tempStrength);
+                    bRefer = (ushort)(((-254.76935184120902))
+                        + (0.8274096064007395 * (temp - 10))
+                        + (115.67994401066147 * Math.Log(temp - 10)));
+                    if (bRefer < 0) bRefer = 0;
+                    else if (bRefer > maxValue) bRefer = maxValue;
                 }
+            }
+            rRefer <<= factor;
+            gRefer <<= factor;
+            bRefer <<= factor;
+            double aplhablend = 0.005;
+            for (int i = 0; i < image.width * image.height; i++)
+            {
+                //red
+                var r = image.imageData[i * 3];
+                image.imageData[i * 3] = (ushort)((image.imageData[i * 3] * (1 - aplhablend)) + (rRefer * aplhablend));
+                var r2 = image.imageData[i * 3];
+                //green
+                var g = image.imageData[(i * 3) + 1];
+                image.imageData[(i * 3) + 1] = (ushort)((image.imageData[(i * 3) + 1] * (1 - aplhablend)) + (gRefer * aplhablend));
+                var g2 = image.imageData[(i * 3) + 1];
+                //blue                
+                var b = image.imageData[(i * 3) + 1];
+                image.imageData[(i * 3) + 2] = (ushort)((image.imageData[(i * 3) + 2] * (1 - aplhablend)) + (bRefer * aplhablend));
+                var b2 = image.imageData[(i * 3) + 1];
+            }
+        }
+
+        public static void scaleColor(ref RawImage currentRawImage, int dark, int saturation, double[] mul)
+        {
+            //TODO remplace to use camera value
+            for (int i = 0; i < currentRawImage.height * currentRawImage.width; i++)
+            {
+                ushort r = currentRawImage.imageData[i * 3];
+                ushort g = currentRawImage.imageData[(i * 3) + 1];
+                ushort b = currentRawImage.imageData[(i * 3) + 2];
+                currentRawImage.imageData[i * 3] = (ushort)(r * mul[0]);
+                currentRawImage.imageData[(i * 3) + 1] = (ushort)(g * mul[1]);
+                currentRawImage.imageData[(i * 3) + 2] = (ushort)(b * mul[2]);
+            }
+        }
+
+        public static void scaleGamma(ref RawImage currentRawImage, double gamma)
+        {
+            //TODO remplace to use camera value
+            int maxValue = (int)Math.Pow(2, currentRawImage.colorDepth) - 1;
+            for (int i = 0; i < currentRawImage.height * currentRawImage.width; i++)
+            {
+                gamma = 1 / gamma;
+                currentRawImage.imageData[i * 3] = (ushort)(maxValue * Math.Pow(currentRawImage.imageData[i * 3] / maxValue, gamma));
+                currentRawImage.imageData[(i * 3) + 1] = (ushort)(maxValue * Math.Pow(currentRawImage.imageData[(i * 3)+1] / maxValue, gamma));
+                currentRawImage.imageData[(i * 3) + 2] = (ushort)(maxValue * Math.Pow(currentRawImage.imageData[(i * 3)+2] / maxValue, gamma));
             }
         }
     }
---
+++ Source/RawParserUWP/RawParserUWP/Model/Image/Effect/ColorScale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RawParserUWP.Model.Format.Image;
+
+namespace RawParserUWP.Model.Image.Effect
+{
+    class ColorScale
+    {
+
+    }
+}
