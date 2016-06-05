3de88b793d9d54122f66065b3d8043ebd25df61a
arimhan <francoischiltz@hotmail.com>
2016-06-06 00:05:45 +0200

    Fix luminance and add first draft of histogram
    Former-commit-id: e6a2eb6483a092ec08002dea083f52c54f63ab46 [formerly def7d39fe3aea03f355f68202b44ba882760e9e3]
    Former-commit-id: 620c72fd7fdcaf0922b68ad9f6c91f8573818a39 [formerly a4cc39db8b4eaf77cd1825c16c3819ce773b410a]
    Former-commit-id: 30093b744106827bd09c282f2110c0469edda1e8
    Former-commit-id: abb818f8e74e746bb5e807fa9406cc61d0bc47f1

--- Source/RawParser/View/Exception/ExceptionDisplay.cs
+++ Source/RawParser/View/Exception/ExceptionDisplay.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 using Windows.UI.Popups;
 
 namespace RawParser.View.Exception
 {
     class ExceptionDisplay
     {
-        public static async void display(string message) {
-            var dialog = new MessageDialog(message);
-            dialog.Title = "Error";
-            dialog.Commands.Add(new UICommand { Label = "Ok", Id = 0 });
-            var res = await dialog.ShowAsync();
-
+        public static async void display(string message)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+             {
+                 //Do some UI-code that must be run on the UI thread.
+                 var dialog = new MessageDialog(message);
+                 dialog.Title = "Error";
+                 dialog.Commands.Add(new UICommand { Label = "Ok", Id = 0 });
+                 var res = await dialog.ShowAsync();
+             });
         }
     }
 }
