d381a49be4ff3f59d694a72ea3fcfbd9abed1341
arimhan <francoischiltz@hotmail.com>
2016-06-05 13:59:48 +0200

    Moved folder
    Former-commit-id: 3790f212c98621cecb0b9c147b6cfe3dd1bb1f4c [formerly 17ae71397465bf533169456957c401902ec7c20e] [formerly bd711950b8e5b0ff8e9dd674328a4347ba0a82bf [formerly bb0ee25effbaf9be8b589d0a6f4470bd9b18627f] [formerly b84dfc448b1607e554a61f2bedc0182629c34d79] [formerly 10e4db97d930f55f7d00d7dccb5051d34a2e4f09 [formerly 59cd987564ccfcdcede9232a0e1c21a4dd2cc62e]] [formerly bd711950b8e5b0ff8e9dd674328a4347ba0a82bf [formerly bb0ee25effbaf9be8b589d0a6f4470bd9b18627f] [formerly b84dfc448b1607e554a61f2bedc0182629c34d79] [formerly 10e4db97d930f55f7d00d7dccb5051d34a2e4f09 [formerly 59cd987564ccfcdcede9232a0e1c21a4dd2cc62e]] [formerly bd711950b8e5b0ff8e9dd674328a4347ba0a82bf [formerly bb0ee25effbaf9be8b589d0a6f4470bd9b18627f] [formerly b84dfc448b1607e554a61f2bedc0182629c34d79] [formerly 10e4db97d930f55f7d00d7dccb5051d34a2e4f09 [formerly 59cd987564ccfcdcede9232a0e1c21a4dd2cc62e]] [formerly 55b96a67c5b759e1cc9347427d004c330fd018ee [formerly eb09c2388ddb7632af935e3034767f0753a40b5f]]]]]
    Former-commit-id: f1493f033ca0e9bd6cfbff159f9535caff71b2f7
    Former-commit-id: 418e0349bb80502d8b3484d0c2821aaf7f7e8754
    Former-commit-id: b84ef36f55c0cabdbae925a18bfa6d8e96c48789
    Former-commit-id: fefeb2b9a70a82b12813b4bba930bf95b31a0af3
    Former-commit-id: 17141dfaede6e2e593b030502356bb5d604c5486 [formerly e445b1fb9b30f705814fc088007d314fde2a03b6]
    Former-commit-id: 3883b299c379682abf8721ec32f3c5ed02c605a3
    Former-commit-id: 1ef94ef63088538f9a2965222141079b9665236c

---
+++ Source/RawParser/Model/Image/Effect/Luminance.cs
@@ -0,0 +1,25 @@
+﻿using RawParserUWP.Model.Format.Image;
+using System;
+
+namespace RawParserUWP.Model.Image.Effect
+{
+    class Luminance
+    {
+        public static void Exposure(ref RawImage image, double value)
+        {
+            ushort maxValue = (ushort)(Math.Pow(2, image.colorDepth) - 1);
+            for (int i = 0; i < image.height * image.width * 3; ++i)
+            {
+                image.imageData[i] += (ushort)(image.imageData[i] * value);
+            }
+        }
+
+        public static void Clip(ref RawImage image, ushort maxValue)
+        {
+            for (int i = 0; i < image.height * image.width; ++i)
+            {
+                if (image.imageData[(i * 3)] > maxValue) image.imageData[(i * 3)] = maxValue;
+            }
+        }
+    }
+}
