60968d700fc4fc7440e90210bbad8a5519a9d81d
arimhan <francoischiltz@hotmail.com>
2016-11-04 12:52:40 +0100

    Doc + DNG
    Add more doc
    Add info for DNG parser
    
    
    Former-commit-id: 2094d17d5292917bb7139b90d84bdda809afcb4e [formerly 96129e9e2b4f50264edcd00e25004f91c2764248] [formerly d7d2ea91bef87f8193d71708e5562d9373890057 [formerly 31e909badc460ecaeb137e4fcc8eeb08277c3242] [formerly 395b4f677ed694b7097eb20f5db1ca7b20432a7f] [formerly 116fcb9159d21d207151dda2ba747c9123a7b61c [formerly 9cdca65a2b6298d448c959cad7c75f88b4efc7f1]] [formerly d7d2ea91bef87f8193d71708e5562d9373890057 [formerly 31e909badc460ecaeb137e4fcc8eeb08277c3242] [formerly 395b4f677ed694b7097eb20f5db1ca7b20432a7f] [formerly 116fcb9159d21d207151dda2ba747c9123a7b61c [formerly 9cdca65a2b6298d448c959cad7c75f88b4efc7f1]] [formerly d7d2ea91bef87f8193d71708e5562d9373890057 [formerly 31e909badc460ecaeb137e4fcc8eeb08277c3242] [formerly 395b4f677ed694b7097eb20f5db1ca7b20432a7f] [formerly 116fcb9159d21d207151dda2ba747c9123a7b61c [formerly 9cdca65a2b6298d448c959cad7c75f88b4efc7f1]] [formerly 8696b006de9daa22d2921ea2822abc079d82a442 [formerly 53e28e126c25524121680ca4cfa452a923f632e9]]]]]
    Former-commit-id: 32dd59b6a178a50cb1779cef8becfaf5f04bbfa3
    Former-commit-id: add9b2104cd9f4cade681ce028112e8c80f09b11
    Former-commit-id: 1fcb9004e9445ff33b365e63d0477151b95ecb94
    Former-commit-id: 0e6ef6f1e6a38ae58828ce3f501dc275efa1e4f8
    Former-commit-id: ace918f6dcc9d1feed76fa5d33b8b976565cb908 [formerly ab25a8e12f44daa4147882ef982f7432da9ed03a]
    Former-commit-id: 5ed3fae1c452b0cf3bfcd36287303bb8d9ae453f
    Former-commit-id: 01ae1d8dbe5d41427caf608a9ddada9c1f0ebe28

--- Source/RawParser/Model/Parser/DNGParser.cs
+++ Source/RawParser/Model/Parser/DNGParser.cs
@@ -10,7 +10,7 @@ namespace RawParser.Parser
 
         public override void Parse(Stream file)
         {
-            readTiffBase(file);
+            readTiffBase(file);           
         }
 
         public override Dictionary<ushort, Tag> parseExif()
@@ -26,6 +26,20 @@ namespace RawParser.Parser
         public override ushort[] parseRAWImage()
         {
             throw new NotImplementedException();
+            //Find the IFD with tag NewSubFileType=0 (it's the raw)
+            //Find the BitsPerSample tag (used to convert from the raw bit/pixel to 16bit/pixel of the raw processor)
+            //Find the SampleFormat tag for integer or float image
+            //Find the compression tag (Compression)
+            //Find the PhotometricInterpretation tag (if RGB or YCbCr)
+            //FInd the orientation for future use
+
+            //Get the linearisation table for decoding
+
+            //Support for croppping active area not in first one
+            //Linearization
+            //Black Subtraction
+            //Rescaling
+            //Clipping            //Map from camera color space to CIEXYZ then RGB
         }
 
         public override byte[] parseThumbnail()
