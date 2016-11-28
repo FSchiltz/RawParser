a9990d9bd1fe250dc02e60262983f139357b8d28
arimhan <francoischiltz@hotmail.com>
2016-11-28 20:32:36 +0100

    Fix for com component
    Former-commit-id: 9f2817564514e67883ea66ea900b6a5de651036c [formerly 80e08675b9e0cf08efe72a2c57995be1f631de8f]
    Former-commit-id: 21ee9cc2463b01908f425e7358ae09c5dcce3b38 [formerly 2775204edee3511b54fe9faff8cdfdaadfa51b3c]
    Former-commit-id: 5b127ca8cbfb240b7bd3be6f43d13fe34b82ee34
    Former-commit-id: e8409651af3a3544be88a74c539281417efe5da5

--- Source/RawParser/Model/Image/JpegHelper.cs
+++ Source/RawParser/Model/Image/JpegHelper.cs
@@ -6,16 +6,17 @@ using Windows.Graphics.Imaging;
 
 namespace RawEditor
 {
+    // Using the COM interface IMemoryBufferByteAccess allows us to access the underlying byte array in an AudioFrame
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
     class JpegHelper
     {
-        // Using the COM interface IMemoryBufferByteAccess allows us to access the underlying byte array in an AudioFrame
-        [ComImport]
-        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        unsafe interface IMemoryBufferByteAccess
-        {
-            void GetBuffer(out byte* buffer, out uint capacity);
-        }
 
         public static SoftwareBitmap getJpegInArray(byte[] im)
         {
