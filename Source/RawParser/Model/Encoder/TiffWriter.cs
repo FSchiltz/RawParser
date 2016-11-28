0105b5b3feb8f74036539377deb84a63cd327bca
arimhan <francoischiltz@hotmail.com>
2016-11-28 20:30:57 +0100

    Fix
    Fix for missing file
    Fix for incorrect folder
    Fix build error
    
    
    Former-commit-id: 512c1a7b2a3737fc0608220dc1599ca91432d3d6 [formerly 8294f74c739024bd44ce6aa772333759ff2f4480] [formerly 094ac9f0510268c1b8fb2811e838c997c3ca20f8 [formerly 03519d1bab26a1f1039ea3385e13ebdb6a6609c3] [formerly 18823da25776bfe78f8b93cf80d72284251ba4f8] [formerly 727d1e46e125e16a44548a0bf994171f9cb682bb [formerly c24b4b82e30b6eb0c8f5b4bb35b1cf23cc650122]] [formerly 094ac9f0510268c1b8fb2811e838c997c3ca20f8 [formerly 03519d1bab26a1f1039ea3385e13ebdb6a6609c3] [formerly 18823da25776bfe78f8b93cf80d72284251ba4f8] [formerly 727d1e46e125e16a44548a0bf994171f9cb682bb [formerly c24b4b82e30b6eb0c8f5b4bb35b1cf23cc650122]] [formerly 269e6a2d0e64f020cde31fe121f5098d3b929371 [formerly 2496eaec93a470be9d7852a07f0f64e38a22affa]]]]
    Former-commit-id: 5c1b5056bf11c26043a73696623fb0a8e21181e6
    Former-commit-id: 89237003b52377e2fa0f1bcb3cfa4b6346264c23
    Former-commit-id: 800ecec463008c810e3d6ea2410be7d06646ef92
    Former-commit-id: 38fd86a223a0055406350986ce1e2ea86d8af841
    Former-commit-id: 711c8970a2de61c6e3d80560a92650f1920dfc28 [formerly 04f979fe065ffa0aaef76be7de7848c8573f4fb4]
    Former-commit-id: a16c05100855c3598977fc2ad2556c8994409125
    Former-commit-id: 07aea0e96f0c4bc3c5e878c1aff909b005eae06f

---
+++ Source/RawParser/Model/Encoder/TiffWriter.cs
@@ -0,0 +1,23 @@
+﻿using RawNet;
+using System.IO;
+
+namespace RawEditor
+{
+    public static class TiffWriter
+    {
+        public static void Write(Stream s, ref RawImage image)
+        {
+            //create the header
+
+            //write the header
+
+            //create the ifd
+
+            //generate the offset
+
+            //write the ifd
+
+            //write the data
+        }
+    }
+}
--- Source/RawParser/View/Pages/MainPage.xaml.cs
+++ Source/RawParser/View/Pages/MainPage.xaml.cs
@@ -24,13 +24,7 @@ using RawNet;
 
 namespace RawEditor
 {
-    [ComImport]
-    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    unsafe interface IMemoryBufferByteAccess
-    {
-        void GetBuffer(out byte* buffer, out uint capacity);
-    }
+
     /// <summary>
     /// The main class of the appliation
     /// </summary>
