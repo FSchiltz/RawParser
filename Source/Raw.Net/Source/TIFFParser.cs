6e55fffb9e5501bd25bd162896f1859ccdbf16e3
arimhan <francoischiltz@hotmail.com>
2016-11-30 14:04:13 +0100

    Fixed DNGDecoder constructor not public
    Former-commit-id: b63c13e822b8b29cff067a2b1bb3b92b210dcb28 [formerly d3d898ef0a86763e623dc618d6db67b45003dda1]
    Former-commit-id: dd9e76841dab1bead32facee4343add1d7256da0 [formerly f96e78665f06e990c8b2658f8b98fb4ec0ad9031]
    Former-commit-id: a37448052ab359a19be0c93b11e0a190615fc45d
    Former-commit-id: 573ec796999db1b36a24ae36d13fe68ca0801196

--- Source/Raw.Net/Source/TIFFParser.cs
+++ Source/Raw.Net/Source/TIFFParser.cs
@@ -97,7 +97,7 @@ namespace RawNet
                 if ((int)(c[0]) > 1)
                     throw new TiffParserException("DNG version too new.");
                 rootIFD = null;
-                return new DngDecoder(root,reader);
+                return new DngDecoder(root,ref reader);
             }
 
             potentials = rootIFD.getIFDsWithTag(TagType.MAKE);
