9bc9dbe018d13c78d0c9ff6efd5ab16b166cf832
arimhan <francoischiltz@hotmail.com>
2016-12-01 22:54:44 +0100

    Improve performance of preview display
    Former-commit-id: 73c87580d62f2518942bda3898ac59761ae89a1b [formerly ee64b5a106b93b8ad185ca307b633b24f45362d0]
    Former-commit-id: fa666afade65fb2d39d894ae0436e52a64a49905 [formerly 4d5559da68b19c2a47bcdc55934307bb1b2f2320]
    Former-commit-id: 67e9aa6b7dbae4e7a705976653ba7dc0b62717eb
    Former-commit-id: 2b5f836bd2d1e1ca8236bcea69736e0d06ed8c0a

--- Source/RawParser/Model/Image/ImageEffect.cs
+++ Source/RawParser/Model/Image/ImageEffect.cs
@@ -1,5 +1,6 @@
 ﻿using RawEditor.Effect;
 using RawNet;
+using Windows.Graphics.Imaging;
 
 namespace RawEditor
 {
@@ -100,14 +101,123 @@ namespace RawEditor
 
                     //clip
                     Luminance.Clip(ref red, ref green, ref blue, maxValue);
-                    image[realPix] = (ushort)red;
-                    image[realPix + 1] = (ushort)green;
-                    image[realPix + 2] = (ushort)blue;
+                    image[realPix] = (byte)red;
+                    image[realPix + 1] = (byte)green;
+                    image[realPix + 2] = (byte)blue;
                     //change gamma from curve 
                     /*
                     image[i * 3] = (ushort)gammaCurve[(int)red];
                     image[(i * 3) + 1] = (ushort)gammaCurve[(int)green];
                     image[(i * 3) + 2] = (ushort)gammaCurve[(int)blue];*/
+                }
+            }
+        }
+
+
+        public unsafe void applyModification(ref ushort[] image, iPoint2D dim, iPoint2D offset, int colorDepth, ref SoftwareBitmap bitmap)
+        {
+            using (BitmapBuffer buffer = bitmap.LockBuffer(BitmapBufferAccessMode.Write))
+            {
+                using (var reference = buffer.CreateReference())
+                {
+                    BitmapPlaneDescription bufferLayout = buffer.GetPlaneDescription(0);
+                    byte* temp;
+                    uint capacity;
+                    ((IMemoryBufferByteAccess)reference).GetBuffer(out temp, out capacity);
+                    maxValue = (uint)(1 << colorDepth);
+                    int shift = colorDepth - 8;
+                    if (!cameraWB)
+                    {
+                        mul = new float[4];
+                        //Balance.calculateRGB((int)temperature, out mul[0], out mul[1], out mul[2]);
+                        mul[0] = (float)(255 / temperature);
+                        mul[1] = (float)(255.0 / tint);
+                        mul[2] = 1;
+                    }
+
+                    //generate the curve            
+                    double[] xCurve = new double[5], yCurve = new double[5];
+                    //mid point
+                    xCurve[2] = maxValue / 2;
+                    yCurve[2] = maxValue / 2;
+                    //shadow
+                    xCurve[0] = 0;
+                    yCurve[0] = shadow * (maxValue / (400));
+                    //hightlight
+                    xCurve[4] = maxValue;
+                    yCurve[4] = maxValue - (hightlight * (maxValue / 400));
+                    //contrast
+                    xCurve[1] = maxValue / 4;
+                    yCurve[1] = ((yCurve[0] + yCurve[2]) / 2) - (maxValue / 200);
+                    xCurve[3] = maxValue * 3 / 4;
+                    yCurve[3] = ((yCurve[2] + yCurve[4]) / 2) + (maxValue / 200);
+                    maxValue--;
+
+                    //interpolate with spline
+                    //double[] contrastCurve = Balance.contrast_curve(shadow, hightlight, 1 << colorDepth);
+                    double[] contrastCurve = Curve.cubicSpline(xCurve, yCurve);
+
+                    //Change the gamma/No more gammaneeded here, the raw should be transformed to neutral gamma before demos
+                    //double[] gammaCurve = Balance.gamma_curve(0.45, 4.5, 2, 8192 << 3);
+
+                    //gammacurve from camera
+                    //double[] gammaCurve = Balance.gamma_curve(camCurve[0] / 100, camCurve[1] / 10, 2, 8192 << 3);
+
+                    for (int y = offset.y; y < dim.y + offset.y; y++)
+                    {
+                        int realY = y * dim.x * 3;
+                        int bufferY = y * dim.x * 4 + +bufferLayout.StartIndex;
+                        for (int x = offset.x; x < dim.x + offset.x; x++)
+                        {
+                            int realPix = realY + (3 * x);
+                            int bufferPix = bufferY + (4 * x);
+                            //get the RGB value
+                            double red = image[realPix],
+                            green = image[realPix + 1],
+                            blue = image[realPix + 2];
+
+                            //convert to linear rgb (not needed, the raw should be in linear already)
+                            /*Balance.sRGBToRGB(ref red, maxValue - 1);
+                            Balance.sRGBToRGB(ref green, maxValue - 1);
+                            Balance.sRGBToRGB(ref blue, maxValue - 1);*/
+
+                            //scale according to the white balance
+                            Balance.scaleColor(ref red, ref green, ref blue, mul);
+                            //clip
+                            Luminance.Clip(ref red, ref green, ref blue, maxValue);
+                            double h = 0, s = 0, l = 0;
+                            //transform to HSL value
+                            Color.rgbToHsl(red, green, blue, maxValue, ref h, ref s, ref l);
+                            //change brightness from curve
+                            //add saturation
+                            l = contrastCurve[(uint)(l * maxValue)] / maxValue;
+                            s *= saturation;
+                            s += vibrance;
+                            l *= exposure;
+                            l += brightness / 100;
+                            //change back to RGB
+                            Color.hslToRgb(h, s, l, maxValue, ref red, ref green, ref blue);
+
+                            //Luminance.Exposure(ref red, ref green, ref blue, exposure);
+                            //Luminance.Brightness(ref red, ref green, ref blue, brightness);
+                            //Balance.scaleGamma(ref red, ref green, ref blue, gamma, maxValue);               
+                            Luminance.Contraste(ref red, ref green, ref blue, maxValue, contrast);
+
+                            //clip
+                            Luminance.Clip(ref red, ref green, ref blue, maxValue);
+                            temp[bufferPix] = (byte)((int)blue >> shift);
+                            temp[bufferPix + 1] = (byte)((int)green >> shift);
+                            temp[bufferPix + 2] = (byte)((int)red >> shift);
+
+                            //set transparency to 255 else image will be blank
+                            temp[bufferPix + 3] = 255;
+                            //change gamma from curve 
+                            /*
+                            image[i * 3] = (ushort)gammaCurve[(int)red];
+                            image[(i * 3) + 1] = (ushort)gammaCurve[(int)green];
+                            image[(i * 3) + 2] = (ushort)gammaCurve[(int)blue];*/
+                        }
+                    }
                 }
             }
         }
