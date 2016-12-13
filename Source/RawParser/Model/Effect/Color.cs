565f941ca611d339e5e293555cf795a5c916682b
Francois Schiltz <francoischiltz@hotmail.com>
2016-12-13 14:05:47 +0100

    Improved performance
    Former-commit-id: 18d32966b661c0ad924cb828c121f625cd3023b0 [formerly 8663f8df3b4347d9b87af85537f7e3662252e4b8]
    Former-commit-id: 882bd22793c8f28557ae07cb64689a9cb117da1f [formerly 4cf38fdc3009bd5043892d060f5c93829f5bde60]
    Former-commit-id: 76870d1a4b3a51a37666bc2fc48a04d452eaed13
    Former-commit-id: 378048a537e37ec68ad6ab8ff934f1120d835079

--- Source/RawParser/Model/Effect/Color.cs
+++ Source/RawParser/Model/Effect/Color.cs
@@ -62,9 +62,10 @@ namespace RawEditor.Effect
         public static double hue2rgb(double p, double q, double t)
         {
             if (t < 0) t += 1;
-            if (t > 1) t -= 1;
+            else if (t > 1) t -= 1;
+
             if (t < 1 / 6.0) return p + (q - p) * 6.0 * t;
-            if (t < 1 / 2.0) return q;
+            if (t < 0.5) return q;
             if (t < 2 / 3.0) return p + (q - p) * (2 / 3.0 - t) * 6.0;
             return p;
         }
