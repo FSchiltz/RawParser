d5fc2ba87b8786813afeba32a5aa6fa34c62266e
Francois Schiltz <francoischiltz@hotmail.com>
2016-12-28 17:39:17 +0100

    Support for jpeg xr export
    Former-commit-id: b9552dbf5f55c7f1e17432e4f035bec04e3d1e0c [formerly 11d20e19c61570c29279cc2cf3daf658cfe3178d] [formerly 90494bdf27c12c3560f3d4438735f485746f2e26 [formerly 57503f8b07f5f879f93064d7c75796cf313ea2b5] [formerly 532d2c8ead1b6cab11fb9db7342a34e5f44aea0a] [formerly cbf949310a11a7950b56273bb8a20c175951d415 [formerly c532aefc041a0224e773ef305512f3174aa5fbc9]] [formerly 90494bdf27c12c3560f3d4438735f485746f2e26 [formerly 57503f8b07f5f879f93064d7c75796cf313ea2b5] [formerly 532d2c8ead1b6cab11fb9db7342a34e5f44aea0a] [formerly cbf949310a11a7950b56273bb8a20c175951d415 [formerly c532aefc041a0224e773ef305512f3174aa5fbc9]] [formerly 63d75c204fc6b2c36859055976c1b56e0b154345 [formerly c3a93e2c8cdc5369275c1b1bf20ffe2a5207a44d]]]]
    Former-commit-id: b5ba4dd6ba731336342301b92e9df31bf87a1603
    Former-commit-id: 9ccce08f6712c6de7a004a47a2a663b6c00954c1
    Former-commit-id: 1aa09f8da2e6b352d56929bf7441460bce73c715
    Former-commit-id: 2d3a909f8b0de3d2a96e18e3cd9351ee6963ec28
    Former-commit-id: b4e6e6ca6b9194e039d496b0e0868d92acaa3370 [formerly 52108edd83e2f5d66f97471458ed5d9881e0cfda]
    Former-commit-id: 05d5fafaca20b9b739dc81e0b9603213d10fe005
    Former-commit-id: 559eab227e04f42c6504c6960f3c1b4d97da4311

--- Source/RawParser/Model/Encoder/SaveHelper.cs
+++ Source/RawParser/Model/Encoder/SaveHelper.cs
@@ -47,6 +47,7 @@ namespace RawEditor
                 temp.Add("PNG image", new List<string>() { ".png" });
                 temp.Add("Tiff image", new List<string>() { ".tiff" });
                 temp.Add("BMP image", new List<string>() { ".bmp" });
+                temp.Add("JpegXR image", new List<string>() { ".jxr" });
                 return temp;
             }
         }
@@ -54,92 +55,55 @@ namespace RawEditor
         public static async void SaveAsync(StorageFile file, SoftwareBitmap bitmap)
         {
             CachedFileManager.DeferUpdates(file);
-            // write to file
-            if (file.FileType == ".jpg" || file.FileType == ".jpeg")
+            using (var filestream = await file.OpenAsync(FileAccessMode.ReadWrite))
             {
-                using (var filestream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                BitmapEncoder encoder = null;
+                // write to file
+                if (file.FileType == ".jxr")
                 {
-                    BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, filestream);
-                    //var x = encoder.BitmapProperties;
-
-                    //Needs to run in the UI thread because fuck performance
-                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                    {
-                        encoder.SetSoftwareBitmap(bitmap);
-                    });
-                    await encoder.FlushAsync();
-                    encoder = null;
-                    bitmap.Dispose();
+                    encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegXREncoderId, filestream);
                 }
-            }
-            else if (file.FileType == ".png")
-            {
-                using (var filestream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                else if (file.FileType == ".jpg" || file.FileType == ".jpeg")
                 {
-                    int[] t = new int[3];
-                    BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, filestream);
-
-                    //Needs to run in the UI thread because fuck performance
-                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    //Do some UI-code that must be run on the UI thread.
-                    encoder.SetSoftwareBitmap(bitmap);
-                });
-                    await encoder.FlushAsync();
-                    encoder = null;
-                    bitmap.Dispose();
+                    encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, filestream);
                 }
-            }
-            else if (file.FileType == ".bmp")
-            {
-                using (var filestream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                else if (file.FileType == ".png")
                 {
-                    int[] t = new int[3];
-                    BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, filestream);
-
-                    //Needs to run in the UI thread because fuck performance
-                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    //Do some UI-code that must be run on the UI thread.
-                    encoder.SetSoftwareBitmap(bitmap);
-                });
-                    await encoder.FlushAsync();
-                    encoder = null;
-                    bitmap.Dispose();
+                    encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, filestream);
                 }
-            }
-            else if (file.FileType == ".tiff" || file.FileType == ".tif")
-            {
-                using (var filestream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                else if (file.FileType == ".bmp")
                 {
-                    int[] t = new int[3];
+                    encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, filestream);
+                }
+                else if (file.FileType == ".tiff" || file.FileType == ".tif")
+                {
                     var propertySet = new BitmapPropertySet();
                     var compressionValue = new BitmapTypedValue(
                         TiffCompressionMode.None, // no compression
                         PropertyType.UInt8
                         );
                     propertySet.Add("TiffCompressionMethod", compressionValue);
-                    BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.TiffEncoderId, filestream, propertySet);
-
-                    //Needs to run in the UI thread because fuck performance
-                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.TiffEncoderId, filestream, propertySet);
+                }
+                else throw new FormatException("Format not supported: " + file.FileType);
+                //Needs to run in the UI thread because fuck performance
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
                 {
                     //Do some UI-code that must be run on the UI thread.
                     encoder.SetSoftwareBitmap(bitmap);
                 });
-                    await encoder.FlushAsync();
-                    encoder = null;
-                    bitmap.Dispose();
-                }
+                await encoder.FlushAsync();
+                encoder = null;
+                bitmap.Dispose();
+                // Let Windows know that we're finished changing the file so
+                // the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                if (status != FileUpdateStatus.Complete && status != FileUpdateStatus.CompleteAndRenamed)
+                    throw new IOException("File could not be saved");
             }
-            else throw new FormatException("Format not supported: " + file.FileType);
-            // Let Windows know that we're finished changing the file so
-            // the other app can update the remote version of the file.
-            // Completing updates may require Windows to ask for user input.
-            FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
-            if (status != FileUpdateStatus.Complete && status != FileUpdateStatus.CompleteAndRenamed)
-                throw new IOException("File could not be saved");
         }
     }
+}
 }
 
