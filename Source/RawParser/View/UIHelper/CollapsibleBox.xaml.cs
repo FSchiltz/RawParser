744fc9cf73bd0b74c52b285b53d8196e62789ef2
arimhan <francoischiltz@hotmail.com>
2017-01-23 23:24:27 +0100

    Various UI fix
    Former-commit-id: 43da6ff13096a511f6fbf88cc2f0d4b772fdddbd [formerly 3b7860dcfccdbb2751b8426697b738d4126a9656] [formerly ce4b4a020b77765feac431637d9372c888161973 [formerly 4ec0e9b22cf967ca4e28796d3711f47d39afdd98] [formerly 82f6420f570d714415f4cdf922d23852be9035ef] [formerly 934d68a920fd962ac572aa4c22a7dc2120e3358a [formerly 9998ed1d1b4cf5b70a8956d7965b27e3a91c4148]] [formerly ce4b4a020b77765feac431637d9372c888161973 [formerly 4ec0e9b22cf967ca4e28796d3711f47d39afdd98] [formerly 82f6420f570d714415f4cdf922d23852be9035ef] [formerly 934d68a920fd962ac572aa4c22a7dc2120e3358a [formerly 9998ed1d1b4cf5b70a8956d7965b27e3a91c4148]] [formerly 699e71a83e14ee41a0a6b238b42bac918e6aa9b7 [formerly ee6848ba3c77952e727ab177e0fb577469164ad2]]]]
    Former-commit-id: b5512a0f4f8b1b42d4766c4c538737ec04f58cae
    Former-commit-id: f96a5e35e696b5c0f93a4dc367c6c6e49f3b1581
    Former-commit-id: c8caaf45595b9c8d7744e2b101a6c79ae69d94cd
    Former-commit-id: 17742345f63cceb499082604cd54ba2dfc65f9db
    Former-commit-id: d359e648b250d60c8b4f375126f1391a22df9523 [formerly d5b3a8441a1b547d8c3ff96ecbaa6fd1b070a881]
    Former-commit-id: 090a7a76b15c772597ec64215854ae7bbbac2a5a
    Former-commit-id: 28a15551562163bec9ce43b1850b863e06016dbd

--- Source/RawParser/View/UIHelper/CollapsibleBox.xaml.cs
+++ Source/RawParser/View/UIHelper/CollapsibleBox.xaml.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas.Effects;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -21,6 +26,7 @@ namespace RawEditor.View.UIHelper
     {
         public bool IsOpen { get; set; } = true;
         public string Text { get { return Header.Text; } set { Header.Text = value; } }
+        private double height;
 
         public CollapsibleBox()
         {
@@ -40,14 +46,49 @@ namespace RawEditor.View.UIHelper
 
         private void Header_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            if (Container.Visibility == Visibility.Collapsed)
+            if (IsOpen)
             {
-                Container.Visibility = Visibility.Visible;
+                var sb = new Storyboard();
+                height = Container.ActualHeight;
+                var animation = new DoubleAnimation()
+                {
+                    To = 0,
+                    From = height,
+                    EnableDependentAnimation = true,
+                    Duration = new Duration(new TimeSpan(0,0,0,0,100))
+                };
+                Storyboard.SetTargetProperty(animation, "Height");
+                Storyboard.SetTarget(animation, Container);
+                sb.Children.Add(animation);
+                sb.Completed += DoubleAnimation_Completed;
+                sb.Begin();
+                VisualStateManager.GoToState(this, "Collapsed", true);
             }
             else
             {
-                Container.Visibility = Visibility.Collapsed;
+                Container.Visibility = Visibility.Visible;
+                var sb = new Storyboard();
+                var animation = new DoubleAnimation()
+                {
+                    To = height,
+                    From = 0,
+                    EnableDependentAnimation = true,
+                    Duration = new Duration(new TimeSpan(0, 0, 0, 0, 100))
+                };
+                Storyboard.SetTargetProperty(animation, "Height");
+                Storyboard.SetTarget(animation, Container);
+                sb.Children.Add(animation);
+                sb.Begin();
+                icon.Symbol = Symbol.BackToWindow;
+                VisualStateManager.GoToState(this, "Expanded", true);
             }
+            IsOpen = !IsOpen;
+        }
+
+        private void DoubleAnimation_Completed(object sender, object e)
+        {
+            Container.Visibility = Visibility.Collapsed;
+            icon.Symbol = Symbol.FullScreen;
         }
     }
 }
