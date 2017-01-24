eac3608c41e0410457e2323fde369afb0e3c5dfc
arimhan <francoischiltz@hotmail.com>
2017-01-24 23:26:46 +0100

    Name clean up
    Former-commit-id: 822aa6e0e896e9571093ea96351f3876cb64779d [formerly a16b4acd8e5697110c2ec808255308211e18c97f] [formerly c66cca8257433a3beb517839bb63149265a31d26 [formerly 6c296c7f9a4b96c25de14c2b05d5e9735873433e] [formerly 73aec066239b017e85269c19cdfa6a237c321bce] [formerly 973a83927df55e47c6ffc49476687c421a9ed21c [formerly 72cdc7cbd8caa1d51fe74bfe8c92b053e7d3b8b7]] [formerly c66cca8257433a3beb517839bb63149265a31d26 [formerly 6c296c7f9a4b96c25de14c2b05d5e9735873433e] [formerly 73aec066239b017e85269c19cdfa6a237c321bce] [formerly 973a83927df55e47c6ffc49476687c421a9ed21c [formerly 72cdc7cbd8caa1d51fe74bfe8c92b053e7d3b8b7]] [formerly c42a6fbb4bf396e61abd09a7be725ed922228cce [formerly 12c40dcf56d18c68e4949b23befb2a1f9b7abc3c]]]]
    Former-commit-id: 60fd0cfdfb606202d147fc7fde50e39c9311a7fc
    Former-commit-id: 8e21206d6a3b672b9642a8d47189da1f03c305fe
    Former-commit-id: de9ed243db7edd4f417095a2e43d221131843f10
    Former-commit-id: 5c641431ff201e426ac5b28927ac8337039f57a4
    Former-commit-id: 6744e29c1e793543c687ea48d4de72ef09050abc [formerly 6a7da946e0dfa7af440aa661bca7e32e0710905a]
    Former-commit-id: 0037dbd56dce53f1fdedff44c343729af9b05339
    Former-commit-id: b58613eb11608c989f51f302642999a2784457bd

--- Source/RawParser/Model/Parser/Base/Common.cs
+++ Source/RawParser/Model/Parser/Base/Common.cs
@@ -17,23 +17,7 @@ namespace RawNet
             if ((_y_temp != 0))
                 x = ~_y_temp >> (32 - n);
             return x;
-        }
-
-        static internal int[] ConvertByteToInt(byte[] array)
-        {
-            int bytePerInt = sizeof(int);
-            int[] temp = new int[array.Length / bytePerInt];
-            for (int i = 0; i < temp.Length; ++i)
-            {
-                temp[i] = BitConverter.ToInt32(array, i * bytePerInt);
-            }
-            return temp;
-        }
-
-        static internal void Memcopy<T>(T[] dest, T[] src)
-        {
-            Memcopy<T>(dest, src, (uint)dest.Length);
-        }
+        }        
 
         internal static void Memcopy<T>(T[] dest, T[] src, uint count)
         {
@@ -44,16 +28,7 @@ namespace RawNet
         {
             for (int i = 0; i < count; i++) dest[i + destOffset] = src[i + srcOffset];
         }
-
-        internal static void ConvertArray(byte[] v, out int[] dest)
-        {
-            dest = new int[v.Length / 4];
-            for (int i = 0; i < v.Length / 4; i++)
-            {
-                dest[i] = v[i] << 24 + v[i + 1] << 16 + v[i + 2] << 8 + v[i + 3];
-            }
-        }
-
+        
         internal static void ConvertArray(object[] v, out byte[] dest)
         {
             dest = new byte[v.Length];
@@ -98,17 +73,6 @@ namespace RawNet
                 if (a[i] != (char)b[i]) return false;
             }
             return true;
-        }
-
-        internal static uint[] ConvertByteToUInt(byte[] array)
-        {
-            int bytePerInt = sizeof(uint);
-            uint[] temp = new uint[array.Length / bytePerInt];
-            for (int i = 0; i < temp.Length; ++i)
-            {
-                temp[i] = BitConverter.ToUInt32(array, i * bytePerInt);
-            }
-            return temp;
         }
 
         internal static bool IsPowerOfTwo(uint x)
