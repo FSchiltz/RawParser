264f52b07935ea58b8b3c8dfb9a68392ffd2ac99
Francois Schiltz <francoischiltz@hotmail.com>
2017-02-06 23:13:33 +0100

    History now works
    Former-commit-id: 3db062c1fb26289e1a67792ed68134b23ce80fcf [formerly 470b78b863efb1927f067957d547b87fdb7ec91c] [formerly 8f0df1bf7164b15380d53e598c1e53127175d405 [formerly ed88fc0fe1d958c684df2da86b50855bf68ca022] [formerly 7542391284c33894568b3df342f0497c3e0f6b30] [formerly 9949b5e62ef327416cae57de4819e257045536b8 [formerly 65d51a8bb45e13b326701a941fdcbcad19f70590]] [formerly 8f0df1bf7164b15380d53e598c1e53127175d405 [formerly ed88fc0fe1d958c684df2da86b50855bf68ca022] [formerly 7542391284c33894568b3df342f0497c3e0f6b30] [formerly 9949b5e62ef327416cae57de4819e257045536b8 [formerly 65d51a8bb45e13b326701a941fdcbcad19f70590]] [formerly f879afb5559ce08ba3ba001aa36762438f404ac5 [formerly 5b6007ac926f4170f32f3cfc126e3cd6cf18641b]]]]
    Former-commit-id: ecfa90cff86319c611a975436a3164e4bab7035e
    Former-commit-id: cab8c04a53aea43549c4e49c586cde673f4d3697
    Former-commit-id: e57e8a5230c80ef6dec52dbbda52243902f5f103
    Former-commit-id: 650283d920cbcfed1d1c5da71ff881b5f4a4063d
    Former-commit-id: a67371718ee042439e963a88f11168e91cf8fb63 [formerly b3fc724349254731a9f812ccb62a5b3f41a4f899]
    Former-commit-id: bdd2a9a83eb263fda3210330b37fcf440f4bc74d
    Former-commit-id: 6a52a0645bbec694ee7c5dccd7ba595e85292f4e

---
+++ Source/RawParser/Model/Settings/HistoryList.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RawEditor.Settings
+{
+    public class HistoryList : ObservableCollection<HistoryObject>
+    {
+        private int CurrentIndex
+        {
+            get { return index; }
+            set
+            {
+                index = value;
+                IsUndoEnabled = !(index < 0);
+                IsRedoEnabled = (index < Count - 1);
+            }
+        }
+        private int index = -1;
+        public event PropertyChangedEventHandler HistoryChanged;
+        public HistoryObject Default { get; set; }
+        private bool undoEnabled = false, redoEnabled = false;
+        public bool IsUndoEnabled
+        {
+            get { return undoEnabled; }
+            set
+            {
+                undoEnabled = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("IsUndoEnabled"));
+            }
+        }
+        public bool IsRedoEnabled
+        {
+            get { return redoEnabled; }
+            set
+            {
+                redoEnabled = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("IsRedoEnabled"));
+            }
+        }
+
+        protected void OnHistoryChanged([CallerMemberName] string propertyName = null)
+        {
+            HistoryChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Undo()
+        {
+            if (CurrentIndex >= 0)
+            {
+                CurrentIndex--;
+                OnHistoryChanged();
+            }
+        }
+
+        public new void Add(HistoryObject history)
+        {
+            CurrentIndex++;
+            Insert(CurrentIndex, history);
+        }
+
+        public new void Clear()
+        {
+            CurrentIndex = -1;
+            IsRedoEnabled = IsUndoEnabled = false;
+            base.Clear();
+        }
+
+        public void Redo()
+        {
+            if (CurrentIndex < Count - 1)
+            {
+                CurrentIndex++;
+                OnHistoryChanged();
+            }
+        }
+
+        public void SetCurrent(int i)
+        {
+            if (i >= 0 && i < Count)
+            {
+                CurrentIndex = i;
+                OnHistoryChanged();
+            }
+        }
+
+        public HistoryObject GetCurrent()
+        {
+            if (CurrentIndex >= 0)
+            {
+                return this[CurrentIndex];
+            }
+            else return Default;
+        }
+    }
+}
