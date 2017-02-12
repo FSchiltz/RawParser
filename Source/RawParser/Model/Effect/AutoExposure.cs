93d190eac1903a58a4766e60642c6fbf9038d98e
arimhan <francoischiltz@hotmail.com>
2017-02-12 16:23:27 +0100

    Remove auto button for now
    Former-commit-id: a2a2ba835a97563925558ebb547fd41b23be57bd [formerly 939ffc5b9a97fd460739a7f0639f87a6902b4236] [formerly a9d2d5553e5b28227aef19dfccf5701600832a85 [formerly 81182e6f50d04c438657181192a4233f103ebfed] [formerly c5936e943b047d37f722af0857439b56b3095e7a] [formerly 11bd8035296e55ed98d21d1f02a6716d9e34cc5f [formerly 0522e014115d5df8e047a8e03eb40d177e3453a9]] [formerly a9d2d5553e5b28227aef19dfccf5701600832a85 [formerly 81182e6f50d04c438657181192a4233f103ebfed] [formerly c5936e943b047d37f722af0857439b56b3095e7a] [formerly 11bd8035296e55ed98d21d1f02a6716d9e34cc5f [formerly 0522e014115d5df8e047a8e03eb40d177e3453a9]] [formerly a59d561540abf87c148028e1c074471b617682f7]]]
    Former-commit-id: 53cccfde277e8ce4b61ad9a1e247a0bf4f2004fe
    Former-commit-id: f648d30217d0b9e245d7271cf09ea5edfb2e8b70
    Former-commit-id: e3e2a118ffcdb53c3b43b62bea5fc7ad9377294b
    Former-commit-id: 82be8a2cf97cc3a635c1c0df1e94a4feca62ccc5
    Former-commit-id: d3d180cd42951e690d711e7d270182edc967e2ce [formerly 26a3520b0a59484d66b96cc1b0a45189aa0ab416]
    Former-commit-id: 7167a1ffad4e3ebfdecdf9e7f45807d924f572b6
    Former-commit-id: 41ee1ee1372f9c18e4315ea2482271fe4ce751f9

--- Source/RawParser/Model/Effect/AutoExposure.cs
+++ Source/RawParser/Model/Effect/AutoExposure.cs
@@ -1,26 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using RawNet;
+using Windows.UI.Xaml.Media;
 
 namespace RawEditor.Effect
 {
     static class AutoExposure
     {
-        public static ImageEffect Get(ImageComponent preview)
+        public static ImageEffect Get(ImageComponent preview, PointCollection histo)
         {
-            //read the image
-
             //find the exposure shift needed
+            //caclute the mean
+            double mean = 0, count = 0;
+            for (int i = 0; i < histo.Count; i++)
+            {
+                mean += histo[i].Y * i;
+                count += histo[i].Y;
+            }
+            mean /= count;
+            var shift = 78 - mean;
+            var sign = Math.Sign(shift);            
+            //get the shift until the mean
 
-            //find the shadow shift$
+            //find the shadow shift
 
             //find the higlight shift
 
             //find the contrast 
-            return new ImageEffect();
+            return new ImageEffect() { Exposure = Math.Log(Math.Abs(shift), 2)*sign };
         }
     }
 }
