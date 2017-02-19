e4e0818abf0ff9e8a6a0e59200973c709a5a56d4
Francois Schiltz <francoischiltz@hotmail.com>
2017-02-19 10:34:11 +0100

    Add more camera profiles
    Former-commit-id: 86d74cbd4e5eaf34613f4396d6a42d45fe0cbb65
    Former-commit-id: 8cf63e4e75a40c3b929c84acf04ae9f35eb47b57 [formerly 3d66445eedc11dc2642a9e2868c13400aedc711e]
    Former-commit-id: 44b8657e36f91d6ebf6f191f24c6e1156f655346
    Former-commit-id: fa9b3b1fb94adfc76139344dd388e3aaaec9677e

--- Source/RawParser/Model/Parser/Decoder/CamRGB.cs
+++ Source/RawParser/Model/Parser/Decoder/CamRGB.cs
@@ -1,14 +1,28 @@
-﻿namespace RawNet.Decoder
+﻿using System.Diagnostics;
+
+namespace RawNet.Decoder
 {
     internal class CamRGB
     {
-        public CamRGB(string name, ushort black, ushort white, double[,] matrix)
+        public CamRGB(string name, ushort black, ushort white, double[,] matrix) : this(name, black, white)
+        {
+            Debug.Assert(matrix.Length == 9);
+            this.matrix = matrix;
+        }
+
+        public CamRGB(string name, ushort black, ushort white, double[] matrix) : this(name, black, white)
+        {
+            Debug.Assert(matrix.Length == 9);
+            this.matrix = new double[,] { { matrix[0], matrix[1], matrix[2] }, { matrix[3], matrix[4], matrix[5] }, { matrix[6], matrix[7], matrix[8] } };
+        }
+
+        public CamRGB(string name, ushort black, ushort white)
         {
             this.name = name;
             this.black = black;
             this.white = white;
-            this.matrix = matrix;
         }
+
         public string name;
         public ushort black = 0, white = 0;
         public double[,] matrix; //XYZ to cam matrice (need transformation)
