be314cf5708e115ab53bbf78bdf6170d95c69462
arimhan <francoischiltz@hotmail.com>
2017-02-21 19:59:19 +0100

    Fix fuji thumbnail
    Former-commit-id: eb6693b5d01f715d944fc443b5869c947727d03a
    Former-commit-id: 26cb3ce3e0b2901d32a37824e2e265c1b85dbf9b [formerly 8f26528b6083b849458ddc2bafc1cbab7d87b44f]
    Former-commit-id: 40ffdb5850d63037608373c06833f3ac12100bb7
    Former-commit-id: cc39bc3124a871db46634014ac0fd5a9352f042b

--- Source/RawParser/Model/Parser/Decoder/Decompressor/BitPump/BitPumpPlain.cs
+++ Source/RawParser/Model/Parser/Decoder/Decompressor/BitPump/BitPumpPlain.cs
@@ -1,81 +1,73 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace RawNet.Decoder.Decompressor
 {
-    // Note: Allocated buffer MUST be at least size+sizeof(uint) large.
-    internal class BitPumpPlain : BitPump
+    // Note: Allocated buffer MUST be at least size+sizeof(int) large.
+    class BitPumpPlain : BitPump
     {
         public override int Offset
         {
-            get
-            {
-                return off - (left >> 3);
-            }
-
+            get { return off >> 3; }
             set
             {
-                if (value >= size)
-                    throw new IOException("Offset set out of buffer");
-
-                left = 0;
-                off = value;
+                off = value * 8;
             }
         }
 
         /*** Used for entropy encoded sections ***/
-        public BitPumpPlain(TiffBinaryReader reader) : this(reader, (uint)reader.Position, (uint)(reader.BaseStream.Length - reader.Position)) { }
-
-        /*** Used for entropy encoded sections ***/
+        public BitPumpPlain(TiffBinaryReader reader) : this(reader, (uint)reader.Position, (uint)reader.RemainingSize) { }
         public BitPumpPlain(TiffBinaryReader reader, long offset, long count)
         {
             MIN_GET_BITS = (BITS_PER_LONG - 7);
-            size = count + sizeof(uint);
-            buffer = new byte[size];
+            size = 8 * count;
+            buffer = new byte[count];
             reader.BaseStream.Position = offset;
             reader.BaseStream.Read(buffer, 0, (int)count);
         }
-
         public BitPumpPlain(byte[] _buffer, uint _size)
         {
-            buffer = _buffer;
-            size = _size + sizeof(uint);
+            MIN_GET_BITS = (BITS_PER_LONG - 7);
+            buffer = (_buffer);
+            size = (_size * 8);
         }
 
-        // Fill the buffer with at least 24 bits
-        public override void Fill() { }
-
-        //get the nbits as an int32
-        public override uint PeekBits(int nbits)
+        unsafe override public uint PeekBit()
         {
-            int shift = left >> 3;
-            uint ret = buffer[shift + 3] | (uint)buffer[shift + 2] << 8 | (uint)buffer[shift + 1] << 16 | (uint)buffer[shift] << 24;
-            ret >>= 32 - nbits - (left & 7);
-            return (uint)(ret & ((1 << nbits) - 1));
+            fixed (byte* t = &buffer[off >> 3])
+            {
+                return (uint)(*(Int32*)t >> (int)(off & 7) & 1);
+            }
         }
 
-        public override uint GetBits(int nbits)
+        unsafe public override uint PeekBits(int nbits)
         {
-            uint ret = PeekBits(nbits);
-            left += nbits;
-            return ret;
-        }
-
-        public override uint PeekBit()
-        {
-            return (uint)(buffer[(left - 1) >> 3] >> ((left - 1) & 0x7)) & 1;
+            fixed (byte* t = &buffer[off >> 3])
+            {
+                return (uint)(*(int*)t >> ((int)off & 7) & ((1 << nbits) - 1));
+            }
         }
 
         public override uint GetBit()
         {
-            uint ret = PeekBit();
-            left++;
-            return ret;
+            uint v = PeekBit();
+            off++;
+            return v;
+        }
+
+        public override uint GetBits(int nbits)
+        {
+            uint v = PeekBits(nbits);
+            off += nbits;
+            return v;
         }
 
         public override void SkipBits(int nbits)
         {
-            left += nbits;
+            off += nbits;
         }
+
+        public override void Fill() { }
     }
 }
