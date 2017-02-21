1a4854dd5a52d1fe4c7f5c3dc1ab86e420f49cff
arimhan <francoischiltz@hotmail.com>
2017-02-21 17:08:01 +0100

    Improve msb bitpump
    Former-commit-id: fba849b4dc7abe900f40803a8d244fc30b6777fd [formerly 90ab39682ca2a8705fc2c797616f31e9acd30a81] [formerly 9cdcb3c507c48e2ffd216447b1a43a0b59ec7c1a [formerly 9421d46a01878e2871290488e4e6f838e3a6790b] [formerly b31e8a567049d1997e0d933038b96cb40fe5aa58] [formerly 883e46f7b9d82fe8de468c2e0563f7d1974e9ac7 [formerly 2ff8729772129b2c62ba1fb92a0f0c547d1ec1c9]] [formerly 9cdcb3c507c48e2ffd216447b1a43a0b59ec7c1a [formerly 9421d46a01878e2871290488e4e6f838e3a6790b] [formerly b31e8a567049d1997e0d933038b96cb40fe5aa58] [formerly 883e46f7b9d82fe8de468c2e0563f7d1974e9ac7 [formerly 2ff8729772129b2c62ba1fb92a0f0c547d1ec1c9]] [formerly 7f29264108597f91ead0040a71fd35d93b72039c]]]
    Former-commit-id: ed8dc1621986f654d83d99970880e6bab605d090
    Former-commit-id: 38bc1d424e85dbdd39ed94437452c3ee529d81df
    Former-commit-id: 323741c767b881441a04096c41bd6e433ec030a2
    Former-commit-id: 00bc1521dcaea1d54a809de2a9b816fa68b30bfa
    Former-commit-id: fb53781a13965fd75215cbf065b8cad7697b6f52 [formerly bceb8f11702aff3731541ea2390decc2dd674fdf]
    Former-commit-id: 51a1c35e6e18e1d6ae60da5c8bd441acfa9939d2
    Former-commit-id: 86ccb6194e26a860829f3159d72d6e8d5c6e0c7b

--- Source/RawParser/Model/Parser/Decoder/Decompressor/BitPump/BitPumpMSB.cs
+++ Source/RawParser/Model/Parser/Decoder/Decompressor/BitPump/BitPumpMSB.cs
@@ -7,8 +7,6 @@ namespace RawNet.Decoder.Decompressor
     // Note: Allocated buffer MUST be at least size+sizeof(uint) large.
     internal class BitPumpMSB : BitPump
     {
-        byte[] current_buffer;
-
         public override int Offset
         {
             get
@@ -34,134 +32,51 @@ namespace RawNet.Decoder.Decompressor
         {
             MIN_GET_BITS = (BITS_PER_LONG - 7);
             size = count + sizeof(uint);
-            buffer = new byte[size];
+            base.buffer = new byte[size];
             reader.BaseStream.Position = offset;
-            reader.BaseStream.Read(buffer, 0, (int)count);
-            Init();
+            reader.BaseStream.Read(base.buffer, 0, (int)count);
         }
 
         public BitPumpMSB(byte[] _buffer, uint _size)
         {
             buffer = _buffer;
             size = _size + sizeof(uint);
-            Init();
-        }
-
-        public void Init()
-        {
-            current_buffer = new byte[24];
-            Fill();
-        }
-
-        private void FillNoCheck()
-        {
-            // Fill in 96 bits
-            //uint[] b = Common.convertByteToUInt(current_buffer);
-            if ((off + 12) > size)
-            {
-                while (left <= 64 && off < size)
-                {
-                    for (int i = left >> 3; i >= 0; i--)
-                        current_buffer[i + 1] = current_buffer[i];
-                    current_buffer[0] = buffer[off++];
-                    left += 8;
-                }
-                while (left <= 64)
-                {
-                    current_buffer[15] = current_buffer[11];
-                    current_buffer[14] = current_buffer[10];
-                    current_buffer[13] = current_buffer[9];
-                    current_buffer[12] = current_buffer[8];
-
-                    current_buffer[11] = current_buffer[7];
-                    current_buffer[10] = current_buffer[6];
-                    current_buffer[9] = current_buffer[5];
-                    current_buffer[8] = current_buffer[4];
-
-                    current_buffer[7] = current_buffer[3];
-                    current_buffer[6] = current_buffer[2];
-                    current_buffer[5] = current_buffer[1];
-                    current_buffer[4] = current_buffer[0];
-
-                    current_buffer[3] = 0;
-                    current_buffer[2] = 0;
-                    current_buffer[1] = 0;
-                    current_buffer[0] = 0;
-                    left += 32;
-                }
-                return;
-            }
-            current_buffer[15] = current_buffer[3];
-            current_buffer[14] = current_buffer[2];
-            current_buffer[13] = current_buffer[1];
-            current_buffer[12] = current_buffer[0];
-
-            current_buffer[11] = buffer[off];
-            current_buffer[10] = buffer[off + 1];
-            current_buffer[9] = buffer[off + 2];
-            current_buffer[8] = buffer[off + 3];
-            off += 4;
-            current_buffer[7] = buffer[off];
-            current_buffer[6] = buffer[off + 1];
-            current_buffer[5] = buffer[off + 2];
-            current_buffer[4] = buffer[off + 3];
-            off += 4;
-            current_buffer[3] = buffer[off];
-            current_buffer[2] = buffer[off + 1];
-            current_buffer[1] = buffer[off + 2];
-            current_buffer[0] = buffer[off + 3];
-            off += 4;
-
-            //convert back b to the current_buffer            
-            left += 96;
         }
 
         // Fill the buffer with at least 24 bits
-        public override void Fill()
-        {
-            if (left < 25) FillNoCheck();
-        }
+        public override void Fill() { }
 
         //get the nbits as an int32
         public override uint PeekBits(int nbits)
         {
-            Fill();
-            int shift = (left - nbits);
-            uint ret = current_buffer[shift >> 3] | (uint)current_buffer[(shift >> 3) + 1] << 8 | (uint)current_buffer[(shift >> 3) + 2] << 16 | (uint)current_buffer[(shift >> 3) + 3] << 24;
-            ret >>= shift & 7;
-            return (uint)(ret & ((1 << (int)nbits) - 1));
+            int shift = left >> 3;
+            uint ret = buffer[shift + 3] | (uint)buffer[shift + 2] << 8 | (uint)buffer[shift + 1] << 16 | (uint)buffer[shift] << 24;
+            ret >>= 32 - nbits - (left & 7);
+            return (uint)(ret & ((1 << nbits) - 1));
         }
 
         public override uint GetBits(int nbits)
         {
             uint ret = PeekBits(nbits);
-            left -= (byte)nbits;
+            left += nbits;
             return ret;
         }
 
         public override uint PeekBit()
         {
-            if (left == 0) Fill();
-            return (uint)(current_buffer[(left - 1) >> 3] >> ((left - 1) & 0x7)) & 1;
+            return (uint)(buffer[(left - 1) >> 3] >> ((left - 1) & 0x7)) & 1;
         }
 
         public override uint GetBit()
         {
             uint ret = PeekBit();
-            left--;
+            left++;
             return ret;
         }
 
         public override void SkipBits(int nbits)
         {
-            int skipn = nbits;
-            while (skipn != 0)
-            {
-                Fill();
-                int n = Math.Min(skipn, left);
-                left -= (byte)n;
-                skipn -= n;
-            }
+            left += nbits;
         }
     }
 }
