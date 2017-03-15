82e28460ab4c33e0bf9e43f7c10a6c4e0e152966
arimhan <francoischiltz@hotmail.com>
2017-03-15 11:35:16 +0100

    Add a donate button

--- Source/RawParser/View/Pages/SettingsView.xaml.cs
+++ Source/RawParser/View/Pages/SettingsView.xaml.cs
@@ -5,10 +5,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.Store;
 using Windows.Data.Pdf;
+using Windows.Services.Store;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
@@ -33,6 +37,7 @@ namespace RawEditor.View.Pages
         public SettingsView()
         {
             InitializeComponent();
+
             var enumval = Enum.GetValues(typeof(DemosaicAlgorithm)).Cast<DemosaicAlgorithm>();
             DemosComboBox.ItemsSource = enumval.ToList();
 
@@ -54,6 +59,82 @@ namespace RawEditor.View.Pages
             };
         }
 
+        public async void Donate()
+        {
+            var context = StoreContext.GetDefault();
+            //do all the donation logic here
+            MessageDialog dialog = new MessageDialog("");
+
+            //now check if the user has already doanted
+            StoreAppLicense appLicense = await context.GetAppLicenseAsync();
+            if (appLicense.AddOnLicenses.TryGetValue("Support", out var license) && license.IsActive)
+            {
+                dialog.Content = "You've already donated for the app. Thank you for your support";
+                dialog.Commands.Add(new UICommand { Label = "Cancel", Id = 0 });
+            }
+            else
+            {
+                //get info about the donation
+
+                // Specify the kinds of add-ons to retrieve.
+                string[] productKinds = { "Durable" };
+                List<String> filterList = new List<string>(productKinds);
+                // Specify the Store IDs of the products to retrieve.
+                string[] storeIds = new string[] { "9pjwtct8t8x1" };
+                StoreProductQueryResult queryResult = await context.GetStoreProductsAsync(filterList, storeIds);
+
+                if (queryResult.ExtendedError != null)
+                {
+                    // The user may be offline or there might be some other server failure.
+                    await new MessageDialog("The purchase was unsuccessful, maybe try again latter", "").ShowAsync();
+                    return;
+                }
+
+                dialog.Content = "You can donate to support the development of the app. This donation doesn't unlock additionnal feature (at least for now). Would you like to continue ?";
+                //Do some UI-code that must be run on the UI thread.               
+                dialog.Commands.Add(new UICommand { Label = "Donate " + queryResult.Products["9pjwtct8t8x1"].Price.FormattedBasePrice, Id = 0 });
+                dialog.Commands.Add(new UICommand { Label = "Cancel", Id = 1 });
+            }
+            dialog.CancelCommandIndex = 1;
+            var pressed = await dialog.ShowAsync();
+
+            //if pressed ok donate
+            if ((int)pressed.Id == 0)
+            {
+                try
+                {
+                    // The customer doesn't own this feature, so
+                    // show the purchase dialog.
+                    var result = await context.RequestPurchaseAsync("9pjwtct8t8x1");
+
+                    //Check the license state to determine if the in-app purchase was successful.
+                    switch (result.Status)
+                    {
+                        case StorePurchaseStatus.Succeeded:
+                            await new MessageDialog("The donation was successful, thank you very much for your support", "").ShowAsync();
+                            break;
+                        case StorePurchaseStatus.AlreadyPurchased:
+                            await new MessageDialog("You've already donated for the app. Thank you for your support", "").ShowAsync();
+                            break;
+                        case StorePurchaseStatus.NetworkError:
+                        case StorePurchaseStatus.ServerError:
+                            await new MessageDialog("The purchase was unsuccessful, please check your internet connection", "").ShowAsync();
+                            break;
+                        case StorePurchaseStatus.NotPurchased:
+                            //user closed, do not bother him more
+                            break;
+                        default:
+                            await new MessageDialog("The purchase was unsuccessful, maybe try again later", "").ShowAsync();
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    await new MessageDialog("The purchase was unsuccessful, maybe try again latter", "").ShowAsync();
+                }
+            }
+        }
+
         private void UpdateView()
         {
             //set value of allcombox to current choosen settings
@@ -61,9 +142,7 @@ namespace RawEditor.View.Pages
             ScaleComboBox.SelectedItem = SettingStorage.PreviewFactor;
             //for demos
             DemosComboBox.SelectedItem = SettingStorage.DemosAlgo;
-
             ThemeComboBox.SelectedItem = SettingStorage.SelectedTheme;
-
             BorderSlider.Value = SettingStorage.ImageBoxBorder * 100;
         }
 
